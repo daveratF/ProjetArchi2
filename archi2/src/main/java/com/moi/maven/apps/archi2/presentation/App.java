77b4f4224639b56ec6080ac923e237ae666e7692
moi2 <daverat.francois@gmail.com>
2017-09-12 12:07:18 +0200

    test 1

---
+++ archi2/src/main/java/com/moi/maven/apps/archi2/presentation/App.java
@@ -0,0 +1,13 @@
+package com.moi.maven.apps.archi2.presentation;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
